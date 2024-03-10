e4653ecb6380bc4e18164db025cb11fcce470029
uphadeharsh45 <128834201+uphadeharsh45@users.noreply.github.com>
2024-03-10 19:23:40 +0530

    modal closes on clicking add customer

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -121,6 +121,8 @@ const Map = () => {
       // console.log("New places:", newPlaces);
       return newPlaces;
     });
+
+  
     // console.log(places) // Add temp to the places array
   };
   useEffect(() => {
@@ -207,7 +209,7 @@ const Map = () => {
         </nav>
       </div>
 
-      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
@@ -237,7 +239,7 @@ const Map = () => {
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-danger" onClick={handleadd}>Add Customer</button>
+              <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={handleadd}>Add Customer</button>
             </div>
           </div>
         </div>
