919a7723bfe30e0cf675d5d721b8ec8d783be8e9
uphadeharsh45 <128834201+uphadeharsh45@users.noreply.github.com>
2024-04-07 15:43:19 +0530

    sms and delete feature added in routewithouttime comp

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -46,32 +46,32 @@ const SignUp = (props) => {
     <img src={logo} alt="Login Image" className="img-fluid mb-4 mx-5 my-3" style={{height:'6.2vh'}} />
     <div className="content" style={{color:'white',fontSize:'2vw',textShadow:'2px 2px #ff0000'}}><ReactTyped strings={["Welcome to RouteMaster"]} typeSpeed={100} showCursor={false}/></div>
     </div>
-      <div className='d-flex justify-content-center' style={{ marginTop: '7%' }}>
+      <div className='d-flex justify-content-center' style={{ marginTop: '4%' }}>
         <div className="container mx-5 d-none d-md-block">
           <img src={signup_img} alt="Login Image" className="img-fluid mb-3" />
         </div>
         <div className='container mx-5' style={{marginTop:'0%',}}>
         <div className="container" style={{padding:'1vh',borderRadius:'2vh',width:'70vh',boxShadow: '0 0 20px #dc3545',backgroundColor:'#1f1c1c'}}>
-        <h2 className='my-4' style={{ color: '#dc3545' }}>SIGNUP</h2>
+        <h2 className='my-2' style={{ color: '#dc3545' }}>SIGNUP</h2>
         <div className="container">
         <form onSubmit={handleSubmit}>
-          <div className="my-4">
+          <div className="my-3">
             <label htmlFor="name" className="form-label" style={{color:'white'}}>Name</label>
             <input type="text" name="name" className="form-control" id="name" aria-describedby="emailHelp" onChange={onChange} style={{borderColor:'#FF3444'}} />
           </div>
-          <div className="my-4">
+          <div className="my-3">
             <label htmlFor="email" className="form-label" style={{color:'white'}}>Email address</label>
             <input type="email" name="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={onChange} style={{borderColor:'#FF3444'}}/>
           </div>
-          <div className="my-4">
+          <div className="my-3">
             <label htmlFor="password" className="form-label" style={{color:'white'}}>Password</label>
             <input type="password" name="password" className="form-control" id="password" onChange={onChange} required minLength={5} style={{borderColor:'#FF3444'}}/>
           </div>
-          <div className="my-4">
+          <div className="my-3">
             <label htmlFor="cpassword" className="form-label" style={{color:'white'}}>Confirm Password</label>
             <input type="password" name="cpassword" className="form-control" id="cpassword" onChange={onChange} required minLength={5} style={{borderColor:'#FF3444'}}/>
           </div>
-          <button type="submit" className="btn btn-danger my-3">Signup</button>
+          <button type="submit" className="btn btn-danger my-1">Signup</button>
           <div className="container my-1" style={{color:'white'}}>
           Already Signed Up?<Link className="nav-link active" aria-current="page" to="/login" style={{color:'#FF3444',textDecoration:'underline'}}>Login</Link>
         </div>
